59f43b8a1e3cf8b8dd42b00a1116f945dd9fc58d
polo2325 <flipflop2325@outlook.com>
2024-09-26 12:50:19 -0400

    added endscreen

--- Apple.java
+++ Apple.java
@@ -9,10 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Apple extends Food
 {
     int speed  = 1;
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public Apple() {
         super(1);
     }
@@ -29,6 +26,7 @@ public class Apple extends Food
         if(getY() >= world.getHeight())
         {
             world.removeObject(this);
+            world.gameOver();
         }
     }
     
